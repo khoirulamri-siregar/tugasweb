675d03227f4a0de6e0c430f5aea7c0c3ff11389b
khoirul amri <khoirulregar.212@gmail.com>
2025-08-30 23:10:35 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('.scroll-animate');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        rootMargin: '0px',
+        threshold: 0.1
+    });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
